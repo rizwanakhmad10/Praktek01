b5682811550bb6fd8860765e030bd670389641c2
Akhmad Rizwan <34591274+rizwanakhmad10@users.noreply.github.com>
2018-04-06 21:31:05 +0800

    Membuat Atribut dan Behavior

--- src/praktek01/Kacamata.java
+++ src/praktek01/Kacamata.java
@@ -10,5 +10,17 @@ package praktek01;
  * @author Smekma
  */
 public class Kacamata {
-    
+   String Merk;
+   String Lensa;
+   String Warna;
+   String Frame;
+   double Harga;
+   
+   void cetakInfo (){
+       System.out.println("Merek :"+Merk);
+       System.out.println("Lensa :"+Lensa);
+       System.out.println("Warna :"+Warna);
+       System.out.println("Frame :"+Frame);
+       System.out.println("Harga :"+Harga);
+   }
 }
